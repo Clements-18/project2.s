01baf0a8687680ee3a4467278ab6d23814dc52e6
Clements-18 <47756963+Clements-18@users.noreply.github.com>
2019-11-14 23:48:29 -0500

    update 2
    syscall added. SW store word and jal - jump and link used on new registers

--- project2.s
+++ project2.s
@@ -4,7 +4,25 @@
  ErrorMessage: .asciiz "Invalid base-35 number."
  Input: .space 1000
  .text
+ 
+ 
 main:
  li $v0, 8 
  la $a0, Input 
  li $a1, 1000
+ 
+ 
+ syscall 
+ addi $sp, $sp, -8
+ sw $a0, 4($sp)
+ sw $ra, 0($sp) 
+ 
+ jal Input_loop
+ lw $t8, 0($sp) 
+ addi $sp, $sp, 4 
+ addi $sp, $sp, -8 
+ sw $t8, 4($sp) 
+ sw $ra, 0($sp)
+ jal Sum
+ j end
+ 
